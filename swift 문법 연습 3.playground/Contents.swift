a64a0bbbcd010b92078e664f83209595567e1925
hsj._.06 <gtw030488@gmail.com>
2022-07-30 02:07:57 +0900

    Swift 문법 : enum 케이스 나누기

---
+++ swift 문법 연습 3.playground/Contents.swift
@@ -0,0 +1,94 @@
+import UIKit
+import Foundation
+
+//MARK: - 조건문
+//var isDarkMode: Bool = true
+//
+//if (isDarkMode == true) {
+//    print("다크모드 입니다")
+//}
+//if (isDarkMode != true) {
+//    print("다크모드가 아닙니다")
+//}
+//if isDarkMode == true {
+//    print("다크모드 입니다")
+//}
+//if isDarkMode {
+//    print("다크모드 입니다")
+//}
+//if !isDarkMode {
+//    print("다크모드가 아닙니다")
+//}
+//
+//var title: String = isDarkMode == true ? "다크모드 입니다" : "다크모드가 아닙니다"
+//print("title: \(title)")
+
+//MARK: - foreach 반복문
+
+//콜렉션: 데이터를 모아둔 것
+//배열, 셋, 딕셔너리, 튜플
+
+//배열
+//var myArray: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+//for item in myArray {
+//    print("item: \(item)")
+//}
+//
+////5보다 큰 것만
+//for item in myArray where item > 5 {
+//    print("item: \(item)")
+//}
+//
+////짝수만
+//for item in myArray where item % 2 == 0 {
+//    print("item: \(item)")
+//}
+////홀수만
+//for item in myArray where item % 2 != 0 {
+//    print("item: \(item)")
+//}
+
+//MARK: - enum
+
+//enum School {
+////    case elementary
+////    case middle
+////    case high
+//    case elementary, middle, high
+//}
+//
+//let yourSchool = School.high
+//print("yourSchool :", yourSchool)
+//
+//enum Grade: Int {
+//    case first = 1
+//    case second = 2
+//}
+//
+//let yourGrade = Grade.second.rawValue
+//print("yourGrade :", yourGrade)
+//
+//enum SchoolDetail {
+//    case elementary(name: String)
+//    case middle(name: String)
+//    case high(name: String)
+//    
+//    func getName() -> String {
+//        switch self {
+//        case .elementary(let name) :
+//            return name
+//        case let .middle(name) :
+//            return name
+//        case .high(let name) :
+//            return name
+//        }
+//        
+//    }
+//}
+//
+//let yourMiddleSchoolName = SchoolDetail.middle(name: "도마중학교")
+//print("yourMiddleSchoolName:", yourMiddleSchoolName)//yourMiddleSchoolName: middle(name: "도마중학교")
+//print("yourMiddleSchoolName:", yourMiddleSchoolName.getName())//yourMiddleSchoolName: 도마중학교
+
+//MARK: -
