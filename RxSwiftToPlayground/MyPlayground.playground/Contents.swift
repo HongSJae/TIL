ba22b0cd7858756b8b130dc8c802c4df951d4f67
hsj._.06 <gtw030488@gmail.com>
2023-01-03 15:14:14 +0900

    RxSwift : . deferred()

--- RxSwiftToPlayground/MyPlayground.playground/Contents.swift
+++ RxSwiftToPlayground/MyPlayground.playground/Contents.swift
@@ -158,3 +158,33 @@ example(of: "create") {
     )
 //    .disposed(by: disposeBag)
 }
+
+example(of: "deferred") {
+    let disposeBag = DisposeBag()
+    
+    // 1
+    var flip = false
+    
+    // 2
+    let factory: Observable<Int> = Observable.deferred{
+        
+        // 3
+        flip.toggle()
+        
+        // 4
+        if flip {
+            return Observable.of(1,2,3)
+        } else {
+            return Observable.of(4,5,6)
+        }
+    }
+    
+    for _ in 0...3 {
+        factory.subscribe(onNext: {
+            print($0, terminator: "")
+        })
+            .disposed(by: disposeBag)
+        
+        print()
+    }
+}
