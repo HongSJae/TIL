3c07ebeab436cb34be97d1a7737539868b873dda
HongSJae <gtw030488@gmail.com>
2024-09-09 16:35:40 +0900

    🎉 :: [iOS] Socket 프젝 세팅

---
+++ iOS/Socket/Socket/Socket/SocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketApp.swift
+//  Socket
+//
+//  Created by 홍승재 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
